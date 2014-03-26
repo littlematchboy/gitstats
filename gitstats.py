b74d1db5ca6eb3b73541075cf6228876882112f3
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-03-26 10:20:48 +0800

    New variable names make more sense

--- gitstats.py
+++ gitstats.py
@@ -3,9 +3,6 @@
 # GPLv2 / GPLv3
 from datetime import datetime
 import getopt
-import os
-import sys
-import time
 from common import *
 from GitDataCollector import GitDataCollector
 
@@ -14,7 +11,7 @@ from common import getgnuplotversion, exectime_external
 from config import conf
 
 if sys.version_info < (2, 6):
-    print >> sys.stderr, "Python 2.6 or higher is required for gitstats"
+    print("Python 2.6 or higher is required for GitStats")
     sys.exit(1)
 
 os.environ['LC_ALL'] = 'C'
@@ -62,15 +59,15 @@ class GitStats:
 
         # if output is not specified, output to web directory (default folder)
         if len(args) == 1:
-            outputpath = conf['output']
+            output_path = conf['output']
         else:
-            outputpath = os.path.abspath(args[-1])
+            output_path = os.path.abspath(args[-1])
 
         try:
-            os.makedirs(outputpath)
+            os.makedirs(output_path)
         except OSError:
             pass
-        if not os.path.isdir(outputpath):
+        if not os.path.isdir(output_path):
             print('FATAL: Output path is not a directory or does not exist')
             sys.exit(1)
 
@@ -78,26 +75,28 @@ class GitStats:
             print('gnuplot not found')
             sys.exit(1)
 
-        print('Output path: %s' % outputpath)
-        cachefile = os.path.join(outputpath, 'gitstats.cache')
+        print('Output path: %s' % output_path)
+        cached_file = os.path.join(output_path, 'gitstats.cache')
 
         if len(args) == 1:
-            gitpaths = args[0:]
+            input_paths = args[0:]
         else:
-            gitpaths = args[0:-1]
+            input_paths = args[0:-1]
 
-        gitpath = gitpaths[0]
+        input_path = input_paths[0]
         # only for 1 path
         # for gitpath in gitpaths:
 
-        print('Git path: %s' % gitpath)
-        project_dir = os.path.basename(os.path.abspath(gitpath))
+        print('Git path: %s' % input_path)
+        project_dir = os.path.basename(os.path.abspath(input_path))
 
+        # loop through all branches, generate report for each branch
         main_branch = 'master'
         lines = getpipeoutput(['git branch -a']).split('\n')
         for line in lines:
             data = GitDataCollector()
 
+            # get local branch name
             if len(line) < 2:
                 continue
             line = line[2:]
@@ -110,11 +109,11 @@ class GitStats:
             os.chdir(project_dir)
 
             print('Collecting data...')
-            data.collect(gitpath)
+            data.collect(input_path)
             os.chdir(rundir)
 
             print('Refining data...')
-            data.saveCache(cachefile)
+            data.saveCache(cached_file)
             data.refine()
 
             os.chdir(rundir)
@@ -122,10 +121,10 @@ class GitStats:
             print('project dir: %s' % project_dir)
             print('Running dir: %s' % rundir)
             print('Generating report...')
-            print('Output dir: %s' % outputpath)
+            print('Output dir: %s' % output_path)
 
             output_suffix = conf['output_suffix']
-            single_project_output_path = os.path.join(outputpath, data.projectname, output_suffix)
+            single_project_output_path = os.path.join(output_path, data.projectname, output_suffix)
 
             time_begin = conf['time_begin']
             time_end = conf['time_end']
